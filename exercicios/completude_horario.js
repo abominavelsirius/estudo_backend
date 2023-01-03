2693855dbcddc3a50449d959c8c3687bed209910
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-01-03 15:33:43 -0300

    atualizando

---
+++ exercicios/completude_horario.js
@@ -0,0 +1,17 @@
+// let hora = 19.12 - 14.42;
+
+// console.log(hora);
+
+
+// let horarioDia = [5, 3, 5, 5, 2, 0, 0];
+
+// const completude = (horarioDia[0] + horarioDia[1] + horarioDia[2] + horarioDia[3] + horarioDia[4] + horarioDia[5] + horarioDia[6]);
+
+// console.log(completude);
+
+const num = [7, 6, 6, 4, 7, 9, 7, 6, 4];
+
+for (i = 0; i < num.lenght; i++) {
+    
+}
+
