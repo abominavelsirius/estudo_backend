be9912ca3d042858fa48709faba500b70f2e5aac
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-01-05 16:03:02 -0300

    ponto interessante sobre tempo de estudo

---
+++ exercicios/inteessante.js
@@ -0,0 +1,13 @@
+const horasPorDia = 4 
+
+console.log("---------------------------------------------------------------")
+console.log(`Se estudando ${horasPorDia} horas por dia:`)
+console.log("---------------------------------------------------------------")
+console.log(`Em 1 semana, será ${horasPorDia * 5} horas;`)
+console.log(`Em 1 mês, será ${horasPorDia * 20} horas;`)
+console.log(`Em 6 meses, será ${horasPorDia * 20 * 6} horas;`)
+console.log(`Em 1 ano, será ${horasPorDia * 20 * 12} horas;`)
+console.log("---------------------------------------------------------------")
+console.log(`1 Ano possui ${24 * 365} horas.`);
+console.log(`Logo seriam ${8760 - 960} horas de qualquer outra coisa sem ser estudo durante o ano.`)
+console.log("---------------------------------------------------------------")
