a354d595a23df31e35f8f52bde976cdbcb1dd2c4
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-01-05 15:35:34 -0300

    atualizando

---
+++ exercicios/minutos.js
@@ -0,0 +1,15 @@
+let minuto = [20, 36, 23, 28, 43];
+const somaMinutos = (minuto[0] + minuto[1] + minuto[2] + minuto[3] +minuto[4]);
+
+console.log(`Foram ${somaMinutos} minutos`);
+
+const emHoras = somaMinutos / 60;
+
+console.log(`Em horas ficam ${emHoras}`);
+
+let resposta = 60 / 5  // 12
+let resposta2 = 12 + 12 // 24 minutos
+
+console.log(`${resposta} então é ${resposta2} Minutos`);
+
+
