45894db65ae6b9f79b5114b134dffc40fa0b672d
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-01-13 16:26:37 -0300

    atualizando diretorios

---
+++ 0.Guanabara/7.Operadores_Relacionais.js
@@ -0,0 +1,37 @@
+// Operadores relacionais
+
+> // maior
+< // menos
+>= // maior ou igual
+<= // menor ou igual
+== // igual
+!= // não igual
+
+
+// toda expressão que tenha um operador relacional o resultado será boolean (true or false)
+
+
+5 > 2 // true
+7 < 4  // false
+8 >= 8 // true
+9 <= 7 // false
+5 == 5 // true
+4 != 4 // false
+
+//exemplos -------------------------------------------------------------------------
+var preço = 500
+var idade = 15
+var curso = HTML
+var n1 = 5
+var n2 = 10
+
+
+
+preço >= 200.50  // o preço é maior ou igual a 250.50?
+idade < 18 // a idade é menor do que 18?
+curso == 'JavaScript' // o curso é JS?
+n1 != n2 // o primeiro número é diferente do segundo?
+
+
+
+
