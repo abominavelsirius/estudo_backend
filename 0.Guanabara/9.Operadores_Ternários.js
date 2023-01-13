ed8a9d4886926c8ed1947a0ea19547dfedcc03ca
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-01-13 16:28:49 -0300

    parando por causa dos raios

---
+++ 0.Guanabara/9.Operadores_Ternários.js
@@ -0,0 +1,15 @@
+// Operadores Ternários
+
+// teste ? true : false
+// ternário porque ele junta 3 operandos
+
+// teste lógico que dá verdadeiro ou falso
+// ? o que vai acontecer quando o teste for verdade
+// : o que vai aconteer quando o teste for falso
+
+// exemplos
+
+var media = 5.5
+
+media >= 7.0 ? "Aprovado" : "Reprovado";
+
