1f949eee0bd87dff9d0566f51d07e86433a3a4c3
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-02-07 02:17:16 -0300

    arrays dentro de uma propriedade de um objeto

---
+++ 3.Objetos/2.Manipulação de objetos/1.tipos.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: "Marina",
+    idade: 16,
+    email: "mariane@firma.com",
+    telefone: [115582852, 21852195],
+};
+
+console.log(cliente.telefone);
+
+
+
+// e quando a pessoa tiver 2 telefones por exemplo?
+// é possivel guardar arrays dentro de uma propriedade de um objeto
+// e tudo que é possivel fazer numa array, será possivel fazer dentro de um objeto
