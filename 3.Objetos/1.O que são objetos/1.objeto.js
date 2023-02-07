0d0c878b0dcb549212dc68faea4e0647d4d96c91
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-02-07 00:46:42 -0300

    comecando os estudos de objetos

---
+++ 3.Objetos/1.O que são objetos/1.objeto.js
@@ -0,0 +1,22 @@
+const listaCPFS = [1111111, 22222, 3333];
+
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "111122223333"];
+
+console.log(informacoesPessoa[1]);
+
+// o objeto é como um copo, ele tem cor, pode ter algo dentro, ele tem altura
+// tem largura, tem sua propria tipagem unica
+// no JS, o objeto é declarado assim:
+
+const objetoPessoa = {
+    idade: 32,
+    nome: "Jose",
+};
+
+
+// dentro das chaves, temos a propriedade e depois dos ":" seu valor
+// ele não precisa ser chamado por indice, e sim, pela propriedade
+// seja ela "idade" ou "nome" como foi declarado acima!!!
+
+
+// agora vamos aprender a como acessar elas.
