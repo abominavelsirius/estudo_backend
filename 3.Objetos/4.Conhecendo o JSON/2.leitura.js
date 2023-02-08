5ea4c2997ba723a4b19d25e57666fab5f3d6ce16
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-02-08 16:18:19 -0300

    lendo um arquivo json

---
+++ 3.Objetos/4.Conhecendo o JSON/2.leitura.js
@@ -0,0 +1,9 @@
+const dados = require("./2.cliente.json");
+// require: vai buscar e trazer o objeto.json pra cá
+
+console.log(dados)
+console.log(typeof dados)
+
+
+
+  // pode alterar valores (n tá em const msm)
