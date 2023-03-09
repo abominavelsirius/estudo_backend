117887c8bcc0440678a82fd4ba27a55df33765eb
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-03-09 17:12:08 -0300

    pensando em algoritmos

--- 4.Algoritmos/1.Pensando em Algoritmos/1.index.js
+++ 4.Algoritmos/1.Pensando em Algoritmos/1.index.js
@@ -4,7 +4,7 @@ const precosLivros = [25, 15, 30, 50, 45, 20];
 let maisBarato = 0;
 
 for (let atual = 0; atual < precosLivros.length; atual++) {
-    if (precosLivros[atual] < precosLivros [maisBarato]) {
+    if (precosLivros[atual] < precosLivros[maisBarato]) {
         maisBarato = atual
     }
 }
