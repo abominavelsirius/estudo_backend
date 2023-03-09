6010295d4635589055a8541d58db1f8641fdfee2
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-03-09 17:38:38 -0300

    realocando

---
+++ 4.Algoritmos/1.Pensando em Algoritmos/3.maiorValorlistaLivros.js
@@ -0,0 +1,12 @@
+const livros = require('./2.1listaLivros');
+
+let maisCaro = 0;
+
+
+for (let atual = 0; atual < livros.length; atual++) {
+ if (livros[atual].preco > livros[maisCaro].preco) {
+   maisCaro = atual;
+ }
+}
+
+console.log(`O livro mais caro custa ${livros[maisCaro].preco} e o título é ${livros[maisCaro].titulo}`)
