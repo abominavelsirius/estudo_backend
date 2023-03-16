523f4a79398ac3ad591fc3f3c280c62ac1ba251c
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-03-16 16:27:41 -0300

    lacos de repeticoes

---
+++ 0.Guanabara/1/23.ex.js
@@ -0,0 +1,26 @@
+var contador = 1
+
+while (contador <= 6) {
+    console.log('tudo bem?')
+    contador++ // c = c + 1
+}
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
+image.png
+
+var c = 1
+
+while  (c <= 3) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+// ==========================
