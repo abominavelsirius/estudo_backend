aee169ca86be17faab683a6e6348c91a037bedfb
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-05-23 16:20:42 -0300

    ordenacao de algoritmo

--- 4.Algoritmos/1.Pensando em Algoritmos/2.2listaLivros.js
+++ 4.Algoritmos/1.Pensando em Algoritmos/2.2listaLivros.js
@@ -4,7 +4,7 @@ const livros = require('./2.1listaLivros');
 let maisBarato = 0;
 
 for (let atual = 0; atual < livros.length; atual++) {
-    if (livros[atual].preco < livros[maisBarato].preco) {
+    if (livros[atual].preco < livros[maisBarato].preco) { // .preco pra chamar a propriedade especifica do objeot
         maisBarato = atual
     }
 }
