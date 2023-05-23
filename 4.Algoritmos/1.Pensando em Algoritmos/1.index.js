e52c25e9cb69ce3ae66da3ee158ca3f1a9940373
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-05-23 15:21:36 -0300

    voltando a estudar, comecando com explicacoes revisando

--- 4.Algoritmos/1.Pensando em Algoritmos/1.index.js
+++ 4.Algoritmos/1.Pensando em Algoritmos/1.index.js
@@ -4,9 +4,24 @@ const precosLivros = [25, 15, 30, 50, 45, 20];
 let maisBarato = 0;
 
 for (let atual = 0; atual < precosLivros.length; atual++) {
-    if (precosLivros[atual] < precosLivros[maisBarato]) {
+    if (precosLivros[atual] < precosLivros[maisBarato]) { 
         maisBarato = atual
     }
 }
 
 console.log(`O livro mais barato custa ${precosLivros[maisBarato]}`)
+
+// atual e mais barato guardam 0, e 0 é o indice 25.
+
+
+// se o array precosLivros na posição [atual], ou seja, no indice 0
+// se ele for menor que o precosLivros na posição [maisBarato], ou seja, no indice 0 
+// então o maisbarato ele receberá o indice atual
+
+// vamos supor quando chegar no 15 o array
+// se o precosLivros[15] for menor que precosLivros[25]
+// então o maisBarato irá guardar o 15 
+
+// quando vier o 30
+// se o precosLivros[30] for menor que o precosLivros[15]
+// não é, então o maisBarato permanecerá guardando o 15
